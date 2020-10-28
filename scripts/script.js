911fd6b8f65683b0c106b5f01c92b3342510b84c
jackieleune <55492440+jackieleune@users.noreply.github.com>
2020-10-28 19:39:06 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+// Microinteractie - favoriet
+
+
+var deDerdeP = document.querySelector("section:nth-of-type(1) p");
+
+deDerdeP.addEventListener("click", veranderMezelf);
+
+function veranderMezelf() {
+  deDerdeP.classList.add("anders");
+
+}
+
+
